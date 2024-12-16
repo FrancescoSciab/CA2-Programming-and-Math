5510e951e29db0c27a45ac513d60e17a19ae486e
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-12-16 12:16:17 +0000

    Bubble sort working

--- src/algorithms/BubbleSort.java
+++ src/algorithms/BubbleSort.java
@@ -24,10 +24,57 @@ public class BubbleSort {
             arr[i] = scanner.nextInt();
         }
 
-        
+        // Display the array before sorting
+        System.out.println("Array before sorting:");
+        printArray(arr);
+
+        // Sort the array using Bubble Sort and count swaps
+        int totalSwaps = bubbleSort(arr);
+
+        // Display the array after sorting
+        System.out.println("Array after sorting:");
+        printArray(arr);
+
+        // Output the total number of swaps
+        System.out.println("Total number of swaps performed: " + totalSwaps);
+
+        scanner.close();
     }
 
-    
+    // Bubble Sort function
+    public static int bubbleSort(int[] arr) {
+        int n = arr.length;
+        int swapCount = 0;
 
-    
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            // Compare adjacent elements
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapCount++;
+                    swapped = true;
+                }
+            }
+
+            // If no swaps were made in this pass, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+        return swapCount;
+    }
+
+    // Function to print an array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }    
 }
