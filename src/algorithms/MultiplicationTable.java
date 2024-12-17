4f8e3da756399c265db38cd8a67fb38e3dfb4782
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-12-17 15:02:30 +0000

    Working on multiplication table. Created 2D array to hold the multiplication table.

---
+++ src/algorithms/MultiplicationTable.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package algorithms;
+
+import java.util.Scanner;
+public class MultiplicationTable {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Input: Size of the multiplication table
+        System.out.print("Enter size of multiplication table: ");
+        int n = scanner.nextInt();
+        
+        // Creating a 2D array to hold the multiplication table
+        int[][] table = new int[n][n];
+    }
+    
+}
